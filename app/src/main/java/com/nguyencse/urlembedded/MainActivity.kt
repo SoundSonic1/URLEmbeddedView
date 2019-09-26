c174fb1433d98a489f69e59ef6e02d249d49f180
Kien Dang Tran <kiendangtran0@gmail.com>
2019-09-26 21:03:41 +0200

    app: MainActivity: use kotlin extensions for view ids

--- app/src/main/java/com/nguyencse/urlembedded/MainActivity.kt
+++ app/src/main/java/com/nguyencse/urlembedded/MainActivity.kt
@@ -1,34 +1,25 @@
 package com.nguyencse.urlembedded
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import com.nguyencse.URLEmbeddedData
 import com.nguyencse.URLEmbeddedView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
-    private var edtURL: EditText? = null
-    private var btnURL: Button? = null
-    private var urlEmbeddedView: URLEmbeddedView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        edtURL = findViewById(R.id.edt_url)
-        btnURL = findViewById(R.id.btn_url)
-        urlEmbeddedView = findViewById(R.id.uev)
-
-        btnURL!!.setOnClickListener {
-            urlEmbeddedView!!.setURL(edtURL!!.text.toString(), object : URLEmbeddedView.OnLoadURLListener {
+        btn_url.setOnClickListener {
+            url_embedded_view.setURL(edt_url.text.toString(), object : URLEmbeddedView.OnLoadURLListener {
                 override fun onLoadURLCompleted(data: URLEmbeddedData) {
-                    urlEmbeddedView!!.title(data.title)
-                    urlEmbeddedView!!.description(data.description)
-                    urlEmbeddedView!!.host(data.host)
-                    urlEmbeddedView!!.thumbnail(data.thumbnailURL)
-                    urlEmbeddedView!!.favor(data.favorURL)
+                    url_embedded_view.title(data.title)
+                    url_embedded_view.description(data.description)
+                    url_embedded_view.host(data.host)
+                    url_embedded_view.thumbnail(data.thumbnailURL)
+                    url_embedded_view.favor(data.favorURL)
                 }
             })
         }
