e3c89788d73381d2b4450b3876e74f63aa790e44
Kien Dang Tran <kiendangtran0@gmail.com>
2019-09-26 20:42:13 +0200

    Add small unit testing for URLEmbeddedTask

---
+++ urlembeddedview/src/test/java/com/example/urlembeddedview/URLEmbeddedTaskTest.kt
@@ -0,0 +1,94 @@
+package com.example.urlembeddedview
+
+import com.nguyencse.URLEmbeddedData
+import com.nguyencse.URLEmbeddedTask
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.newSingleThreadContext
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.setMain
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+
+/**
+ * Test class
+ */
+class URLEmbeddedTaskTest {
+
+    /**
+     * Override Dispatchers.Main
+     * see: https://github.com/Kotlin/kotlinx.coroutines/tree/master/kotlinx-coroutines-test
+     */
+    private val mainThreadSurrogate = newSingleThreadContext("UI thread")
+
+    private var testData: URLEmbeddedData? = null
+
+    val task = URLEmbeddedTask(object : URLEmbeddedTask.OnLoadURLListener {
+        override fun onLoadURLCompleted(data: URLEmbeddedData) {
+            testData = data
+        }
+    })
+
+    /**
+     * Reset data before each test
+     */
+    @Before
+    fun setUp() {
+        Dispatchers.setMain(mainThreadSurrogate)
+        testData = null
+    }
+
+    @Test
+    fun testUrlEmbeddedData() {
+
+        val expectedData = URLEmbeddedData().apply {
+            host = "github.com"
+            title = "Build software better, together"
+            // there are multiple images on the website, we take the last one
+            thumbnailURL = "https://github.githubassets.com/images/modules/open_graph/github-octocat.png"
+            favorURL = "https://www.google.com/s2/favicons?domain=github.com"
+            description = "GitHub is where people build software. More than 40 million people use GitHub to discover, fork, and contribute to over 100 million projects."
+        }
+
+        runBlocking {
+            // wait for job to finish
+            task.fetchData("https://github.com/").join()
+        }
+        Assert.assertEquals(expectedData.title, testData?.title)
+        Assert.assertEquals(expectedData.host, testData?.host)
+        Assert.assertEquals("Expected octacat image", expectedData.thumbnailURL, testData?.thumbnailURL)
+        Assert.assertEquals(expectedData.favorURL, testData?.favorURL)
+        Assert.assertEquals(expectedData.description, testData?.description)
+
+    }
+
+    @Test
+    fun testInvalidUrl() {
+        val emptyTask = URLEmbeddedTask(object : URLEmbeddedTask.OnLoadURLListener {
+            override fun onLoadURLCompleted(data: URLEmbeddedData) {
+                testData = data
+            }
+        })
+
+        runBlocking {
+            emptyTask.fetchData("").join()
+        }
+        Assert.assertEquals("", testData?.title)
+        Assert.assertEquals("", testData?.host)
+        Assert.assertEquals("", testData?.thumbnailURL)
+        Assert.assertEquals("", testData?.favorURL)
+        Assert.assertEquals("", testData?.description)
+
+        // TODO: make test work with invalid urls
+        runBlocking {
+            task.fetchData("https://github.c").join()
+        }
+        /*Assert.assertEquals("", testData?.title)
+        Assert.assertEquals("", testData?.host) */
+        Assert.assertEquals("", testData?.thumbnailURL)
+        Assert.assertEquals("", testData?.favorURL)
+        Assert.assertEquals("", testData?.description)
+
+    }
+
+}
